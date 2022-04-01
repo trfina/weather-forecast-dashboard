cfb278c70f840d0a111d1e73b82cd0bb6cac3bc6
Tammy Fina <finatammy@gmail.com>
2022-04-01 12:08:53 -0400

    edit background color

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,13 +1,49 @@
 //const apiKey = "60a440d89263b43b0c735288b5181bda";
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#cityname");
+var weatherContainerEl = document.querySelector("#weather-container");
+var weatherSearchTerm = document.querySelector("#weather-search-term");
+var weatherHumidity = document.querySelector("#weather-humidity")
 
-var getCityWeather = function() {
-    console.log("function was called");
+// if (cityname) {
+//     getLocation(cityname);
+//     weatherContainerEl.textContent = "";
+//     nameInputEl.value = "";
+//     } else {
+//     alert("Please enter a City");
+//     }
+//     console.log(cityname);
+// };
 
+var getCityWeather=function(){
+    console.log("function was called")
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?lang=en&units=imperial&q=columbus" + "&appid=60a440d89263b43b0c735288b5181bda";
     
     fetch(apiUrl)
-    console.log(data);
-    
+    .then(function(response) {
+        if (response.ok) {
+            console.log(response);
+            response.json().then(function(data) {
+                console.log(data);
+                //displayWeather(data);
+        });
+        } else {
+            alert("Error: " + response.statusText);
+        }
+    })
+        .catch(function(error) {
+        alert("Unable to connect");
+    });   
 };
+
+var displayWeather = function(data, searchTerm, humidity) {
+    weatherSearchTerm.textContent = searchTerm;
+    weatherHumidity.textContent = humidity;
+
+    // console.log("City: " + data.name);
+    // console.log("Humidity: " + data.main.humidity);
+    // console.log("Wind Speed: " + data.wind.speed);
+    // console.log("data",data.weather.icon);
+}; 
 
 getCityWeather();
